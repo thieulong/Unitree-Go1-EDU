c4e5398eaa4cc9f62069e93facf7602fc4bdcfb8
Thieu Long <59945736+thieulong@users.noreply.github.com>
2024-01-11 16:00:30 +1100

    Update image_segment.py

--- Image-Segmentation/image_segment.py
+++ Image-Segmentation/image_segment.py
@@ -16,17 +16,16 @@ from mit_semseg.utils import colorEncode
 
 class SemanticSegmentationNode:
     def __init__(self):
-        
+        rospy.init_node('semantic_segmentation_node')
+
         self.color_pub = rospy.Publisher('/color_output', Image, queue_size=10)
         self.grayscale_pub = rospy.Publisher('/grayscale_output', Image, queue_size=10)
         self.preprocessed_pub = rospy.Publisher('/preprocessed_image', Image, queue_size=10)
 
-        # Load the color map and class names
         colors_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data/color150.mat')
         self.colors = scipy.io.loadmat(colors_file)['colors']
         self.grouped_class_indices = [3, 6, 9, 11, 13, 28, 46, 54]
 
-        # Load network models
         encoder_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ckpt/ade20k-resnet50dilated-ppm_deepsup/encoder_epoch_20.pth')
         decoder_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ckpt/ade20k-resnet50dilated-ppm_deepsup/decoder_epoch_20.pth')
         self.net_encoder = ModelBuilder.build_encoder(arch='resnet50dilated', fc_dim=2048, weights=encoder_path)
@@ -34,14 +33,13 @@ class SemanticSegmentationNode:
         self.crit = torch.nn.NLLLoss(ignore_index=-1)
         self.segmentation_module = SegmentationModule(self.net_encoder, self.net_decoder, self.crit)
         self.segmentation_module.eval()
-        # self.segmentation_module.cuda()
 
-        # Subscribe to the raw_image topic
         rospy.Subscriber("/camera/front/left/image_rect_color", Image, self.image_callback)
 
     def imgmsg_to_cv2(self, img_msg):
         if img_msg.encoding != "bgr8":
-            rospy.logerr("Error in image type")
+            rospy.logerr("Unsupported image encoding: {}".format(img_msg.encoding))
+            return None
         dtype = np.dtype("uint8")
         dtype = dtype.newbyteorder('>' if img_msg.is_bigendian else '<')
         image_opencv = np.ndarray(shape=(img_msg.height, img_msg.width, 3), dtype=dtype, buffer=img_msg.data)
@@ -56,49 +54,38 @@ class SemanticSegmentationNode:
         image.width = cv_image.shape[1]
         image.encoding = encoding
         image.is_bigendian = 0
-        image.step = len(image.data) // image.height
-        image.data = cv_image.tostring()
+        image.step = cv_image.shape[1] * cv_image.shape[2]
+        image.data = cv_image.tobytes()
         return image
 
     def image_callback(self, msg):
-        # cv_image = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, 3)
         cv_image = self.imgmsg_to_cv2(msg)
-        # print("PASSED!")
-        # Publish preprocessed image to "/preprocessed_image" topic
-        # preprocessed_image_msg = self.(cv_image, encoding="bgr8")
-        # self.preprocessed_pub.publish(preprocessed_image_msg)
+        if cv_image is None:
+            return
 
-        img_data = torchvision.transforms.ToTensor()(cv_image)[None]#.cuda
+        img_data = torchvision.transforms.ToTensor()(cv_image)[None].numpy()
 
-        # Run the segmentation at the highest resolution.
         with torch.no_grad():
             scores = self.segmentation_module({'img_data': img_data}, segSize=img_data.shape[2:])
 
-        # Get the predicted class labels
         _, pred = torch.max(scores, dim=1)
         pred = pred.cpu().numpy()
 
-        # Filter class indices
         pred_filtered = np.zeros_like(pred)
         for index in self.grouped_class_indices:
             pred_filtered[pred == index] = 1
 
-        print(pred_filtered)
-        print(pred_filtered.shape)
+        # Uncomment the following line if you want to publish the preprocessed image
+        # preprocessed_image_msg = self.cv2_to_imgmsg(cv_image, encoding="bgr8")
+        # self.preprocessed_pub.publish(preprocessed_image_msg)
 
-        # # Visualize the result for the grouped classes in color
-        # color_segmented = colorEncode(pred_filtered, self.colors).astype(np.uint8)
         color_segmented = colorEncode(scores, self.colors)
-        
-
         color_segmented_image_msg = self.cv2_to_imgmsg(color_segmented, encoding="bgr8")
         self.color_pub.publish(color_segmented_image_msg)
 
-        # Visualize the result for the grouped classes in grayscale
         grayscale_segmented_image_msg = self.cv2_to_imgmsg(pred_filtered, encoding="mono8")
         self.grayscale_pub.publish(grayscale_segmented_image_msg)
 
 if __name__ == "__main__":
-    rospy.init_node('semantic_segmentation_node')
     segmentation_node = SemanticSegmentationNode()
     rospy.spin()
