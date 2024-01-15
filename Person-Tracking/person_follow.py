ad58f319585f1d26e9c83ad478ccdfdad5b2d76c
thieulong <thieulongph@gmail.com>
2024-01-15 11:38:30 +1100

    Added boundary boxes for person movement limit

--- Person-Tracking/person_follow.py
+++ Person-Tracking/person_follow.py
@@ -4,104 +4,133 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64MultiArray
 import math
 
-# assume person is standing on the left and in front of the robot
-THRES_X = 0.2
-THRES_Y = 0.3
-TOLER_X = 0.3
-TOLER_Y = 0.4
-X_UP = THRES_X + TOLER_X
-X_LO = THRES_X - TOLER_X
-Y_R = THRES_Y - TOLER_Y
-Y_L = THRES_Y + TOLER_Y
-
-X_FAC = 0.5
-YAW_FAC = 1
-DBL_YAW_FAC = 1.5*YAW_FAC
-zero_up = 0.01
-zero_down = -0.01
-
 class Person_follow:
     def __init__(self):
         pub__topic = "/cmd_vel"
         sub__topic = "/person_tracking"
 
         self.pub_ = rospy.Publisher(pub__topic, Twist, queue_size=10)
-        self.sub_ = rospy.Subscriber(sub__topic, Float64MultiArray, self.follow_callback)
+        self.sub_ = rospy.Subscriber(sub__topic, Float64MultiArray, self.follow_callback(mode="left"))
         self.vel_msg = Twist()
 
-    def follow_callback(self, msg):
+        self.THRES_X = 0.2
+        self.THRES_Y = 0.3
+        self.TOLER_X = 0.3
+        self.TOLER_Y = 0.4
+
+        self.X_FAC = 0.5
+        self.YAW_FAC = 1
+        self.DBL_YAW_FAC = 1.5*self.YAW_FAC
+
+        self.zero_up = 0.01
+        self.zero_down = -0.01
+
+    def follow_callback(self, msg, mode):
         
         if (msg.data == [0,0,0,0]):
             self.vel_msg.linear.x = 0
             self.vel_msg.angular.z = 0
+
         else:
-            curx = msg.data[0] # old x 
-            cury = msg.data[1] # old y
-            newx = msg.data[2] # new x 
-            newy = msg.data[3] # new y
-
-        
-
-            # if (cury >= Y_L):
-            #     if (curx < X_LO):
-            #         self.vel_msg.linear.x = -(curx - X_LO)/X_FAC
-            #         self.vel_msg.angular.z = 1.0/YAW_FAC
-            #         # self.pub_.publish(self.vel_msg)
-            #         # while (cury > Y_L):    
-            #         #     continue
-            #     elif (curx > X_UP):
-            #         self.vel_msg.linear.x = (curx - X_UP)/ X_FAC
-            #         self.vel_msg.angular.z = (cury - Y_L)/ YAW_FAC * 0.9
-            #     else:
-            #         self.vel_msg.linear.x = 0.5
-            #         self.vel_msg.angular.z = 0.8    
-            # elif (cury <= Y_R):
-            #     if (curx < X_LO):
-            #         self.vel_msg.linear.x = 0.0
-            #         self.vel_msg.angular.z = 0.0
-            #     elif (curx > X_UP):
-            #         self.vel_msg.linear.x = (curx - X_UP)/ X_FAC
-            #         self.vel_msg.angular.z = (cury - Y_R)/ YAW_FAC * 1.5
-            #     else:
-            #         self.vel_msg.angular.z = -1.0
-            # else:
-            #     self.vel_msg.angular.z = 0
-            #     if (curx < X_LO):
-            #         self.vel_msg.linear.x = (curx - X_LO)/ X_FAC
-            #     elif (curx > X_UP):
-            #         self.vel_msg.linear.x = (curx - X_UP)/ X_FAC
-            #     else:
-            #         self.vel_msg.linear.x = 0
+            curx = msg.data[0] 
+            cury = msg.data[1] 
+            newx = msg.data[2]  
+            newy = msg.data[3] 
 
             diffx = 2*newx - curx
             diffy = 2*newy - cury
-            self.vel_msg.linear.x = X_FAC*math.sqrt(diffx**2 + diffy**2)
-
-
+            self.vel_msg.linear.x = self.X_FAC*math.sqrt(diffx**2 + diffy**2)
 
             if diffx > 0:
-                self.vel_msg.angular.z = YAW_FAC*math.atan(diffy/diffx)
+                self.vel_msg.angular.z = self.YAW_FAC*math.atan(diffy/diffx)
             elif diffx < 0:
-                self.vel_msg.angular.z = -DBL_YAW_FAC*math.atan(diffy/diffx)
+                self.vel_msg.angular.z = -self.DBL_YAW_FAC*math.atan(diffy/diffx)
             else:
-                self.vel_msg.angular.z = abs(diffy)*YAW_FAC*(math.pi/2)
-
-
+                self.vel_msg.angular.z = abs(diffy)*self.YAW_FAC*(math.pi/2)
 
             # if (curx < X_UP and curx > X_LO and cury < Y_L and cury > Y_R) or (curx < X_UP and curx > X_LO and cury < -Y_L and cury > -Y_R):
-            if (curx < X_UP and curx > X_LO and cury < Y_L and cury > Y_R):
-                self.vel_msg.linear.x = 0
-                self.vel_msg.angular.z = 0
+            if mode == 'left':
+                X_UP = 0.2
+                X_LO = -0.4
+                Y_R = -0.1
+                Y_L = 0.7
+                if self.person_in_range(curx, cury, X_UP, X_LO, Y_L, Y_R):
+                   self.vel_msg.linear.x = 0
+                   self.vel_msg.angular.z = 0
 
-            if diffx > zero_down and diffx < zero_up and diffy > zero_down and diffy < zero_up:
-                self.vel_msg.linear.x = 0
-                self.vel_msg.angular.z = 0
+            elif mode == 'right':
+                X_UP = 0.5
+                X_LO = -0.1
+                Y_R = -0.7
+                Y_L = 0.1
+                if self.person_in_range(curx, cury, X_UP, X_LO, Y_L, Y_R):
+                   self.vel_msg.linear.x = 0
+                   self.vel_msg.angular.z = 0
+
+            elif mode == 'front':
+                X_UP = 0.8
+                X_LO = -0.1
+                Y_R = -0.3
+                Y_L = 0.3
+                if self.person_in_range(curx, cury, X_UP, X_LO, Y_L, Y_R):
+                   self.vel_msg.linear.x = 0
+                   self.vel_msg.angular.z = 0
+
+            elif mode == 'front_left':
+                X_UP = 0.8
+                X_LO = 0
+                Y_R = 0
+                Y_L = 0.6
+                if self.person_in_range(curx, cury, X_UP, X_LO, Y_L, Y_R):
+                   self.vel_msg.linear.x = 0
+                   self.vel_msg.angular.z = 0
+
+            elif mode == 'front_right':
+                X_UP = 0.8
+                X_LO = 0
+                Y_R = -0.6
+                Y_L = 0
+                if self.person_in_range(curx, cury, X_UP, X_LO, Y_L, Y_R):
+                   self.vel_msg.linear.x = 0
+                   self.vel_msg.angular.z = 0
+
+            elif mode == 'back':
+                X_UP = 0.1
+                X_LO = -0.8
+                Y_R = -0.3
+                Y_L = 0.3
+                if self.person_in_range(curx, cury, X_UP, X_LO, Y_L, Y_R):
+                   self.vel_msg.linear.x = 0
+                   self.vel_msg.angular.z = 0
+
+            elif mode == 'back_left':
+                X_UP = 0.1
+                X_LO = -0.8
+                Y_R = 0
+                Y_L = 0.6
+                if self.person_in_range(curx, cury, X_UP, X_LO, Y_L, Y_R):
+                   self.vel_msg.linear.x = 0
+                   self.vel_msg.angular.z = 0
+
+            elif mode == 'back_right':
+                X_UP = 0.1
+                X_LO = -0.8
+                Y_R = -0.6
+                Y_L = 0
+                if self.person_in_range(curx, cury, X_UP, X_LO, Y_L, Y_R):
+                   self.vel_msg.linear.x = 0
+                   self.vel_msg.angular.z = 0
+
+            # if diffx > zero_down and diffx < zero_up and diffy > zero_down and diffy < zero_up:
+            #     self.vel_msg.linear.x = 0
+            #     self.vel_msg.angular.z = 0
+                
             print(self.vel_msg.linear.x, self.vel_msg.angular.z)
             self.pub_.publish(self.vel_msg)
 
-
-        
-
+    def person_in_range(self, curx, cury, X_UP, X_LO, Y_L, Y_R):
+        if (curx < X_UP and curx > X_LO and cury < Y_L and cury > Y_R): # if user is within these boundaries, stop movement
+            return True
 
 
 if __name__ == '__main__':
